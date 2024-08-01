7a2434926ac7713d8a953d36689149fb061497ee
Moustapha Ndiaye <moustaphandiaye.niani@gmail.com>
2024-08-01 17:19:32 +0200

    Delete faces directory

--- faces/faces.py
+++
@@ -1,9 +0,0 @@
-def main():
-    emoji_in = input('Input a phrase with emoji: ')
-    print(convert(emoji_in))
-
-def convert(emoji):
-    emoticon = emoji.replace(':)', '🙂').replace(':(', '🙁')
-    return emoticon
-
-main()
